99fca4e82b38e9d9bbe33feabd687f142e0cd25c
Vaishnavi <vaishnavigaikwad3939@gmail.com>
2024-06-15 15:07:57 +0530

    Create students and examinations query

---
+++ SQL-50/BASIC JOINS/Students and Examinations/Query.sql
@@ -0,0 +1,29 @@
+--Write your MySQL query statement below
+SELECT
+    s.student_id,
+    s.student_name,
+    sub.subject_name,
+    COUNT(e.subject_name) AS attended_exams
+FROM
+    Students s
+    JOIN Subjects sub
+    LEFT JOIN Examinations e ON s.student_id = e.student_id
+    AND sub.subject_name = e.subject_name
+GROUP BY
+    s.student_id,
+    sub.subject_name
+ORDER BY
+    student_id,
+    subject_name
+
+
+--Another query
+
+/*SELECT s.student_id, s.student_name, sub.subject_name,
+       COALESCE(COUNT(e.subject_name), 0) AS attended_exams
+FROM Students s
+CROSS JOIN Subjects sub
+LEFT JOIN Examinations e ON s.student_id = e.student_id AND sub.subject_name = e.subject_name
+GROUP BY s.student_id, s.student_name, sub.subject_name
+ORDER BY s.student_id, sub.subject_name;
+*/
